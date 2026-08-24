c970f0e827c5d430b577e3fbc38480e272f8e78e
alex <907785012@qq.com>
2026-08-24 12:16:33 +0800

    complete section1-4

--- part1/section4-nativewind/final/src/app/index.tsx
+++ part1/section4-nativewind/final/src/app/index.tsx
@@ -1,28 +1,15 @@
 import { useRouter } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function Index() {
   const router = useRouter();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.hint}>
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-2xl text-red-500'>
         Edit src/app/index.tsx to edit this screen.
       </Text>
       <Text onPress={() => router.navigate('/about')}>Go To About</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  hint: {
-    fontSize: 30,
-    color: 'red',
-  },
-});
