1c16b275a9e7d07027185d62d58ecbc6cf7ebf7d
alex <907785012@qq.com>
2026-08-25 23:52:29 +0800

    add starter of section1-6

---
+++ part1/section6-tabs/final/src/app/about.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const AboutScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text className='text-center text-4xl'>AboutScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default AboutScreen;
