3e569695748b0019c7728cc560275a9a5a6ba976
alex <907785012@qq.com>
2026-08-25 12:59:39 +0800

    add starter of section1-5

---
+++ part1/section5-stack-tabs/starter/src/app/index.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Index() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.hint}>
+        Edit src/app/index.tsx to edit this screen.
+      </Text>
+      <Text onPress={() => router.navigate('/about')}>Go To About</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  hint: {
+    fontSize: 30,
+    color: 'red',
+  },
+});
