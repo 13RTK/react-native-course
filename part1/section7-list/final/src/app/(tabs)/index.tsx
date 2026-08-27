83dc7d522821c09560f4557ac19a78fed6c555b6
alex <907785012@qq.com>
2026-08-27 16:26:50 +0800

    complete section1-7

--- part1/section7-list/final/src/app/(tabs)/index.tsx
+++ part1/section7-list/final/src/app/(tabs)/index.tsx
@@ -1,16 +1,39 @@
-import { useRouter } from 'expo-router';
-import { Text } from 'react-native';
+import { getFeeds } from '@/services/apiFeed';
+import { Host, List, ListItem } from '@expo/ui';
+import { useQuery } from '@tanstack/react-query';
+import { ActivityIndicator } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
-  const router = useRouter();
+  const {
+    data: feeds,
+    isLoading,
+    isSuccess,
+    error,
+  } = useQuery({
+    queryKey: ['feeds'],
+    queryFn: getFeeds,
+  });
+
+  if (isLoading) {
+    return (
+      <SafeAreaView className='flex-1 flex justify-center items-center'>
+        <ActivityIndicator size='large' />
+      </SafeAreaView>
+    );
+  }
 
   return (
-    <SafeAreaView className='flex-1 items-center justify-center'>
-      <Text className='text-2xl text-red-500'>
-        Edit src/app/index.tsx to edit this screen.
-      </Text>
-      <Text onPress={() => router.navigate('/about')}>Go To About</Text>
-    </SafeAreaView>
+    <Host style={{ flex: 1 }}>
+      {isSuccess && (
+        <List>
+          {feeds.map((feed) => (
+            <ListItem supportingText={feed.description} key={feed.id}>
+              {feed.title}
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </Host>
   );
 }
