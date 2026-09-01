99a7b05d1ab207dd896ec6328ca52002be2f0df6
alex <907785012@qq.com>
2026-09-01 15:02:59 +0800

    add starter of final02

---
+++ galex/final02-take-photo/src/app/(tabs)/gallery.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const GalleryScreen = () => {
+  return (
+    <View>
+      <Text>GalleryScreen</Text>
+    </View>
+  );
+};
+
+export default GalleryScreen;
